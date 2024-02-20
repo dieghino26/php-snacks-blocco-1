cee116c8a0b77bed58fcee1225d3aa6fb38411cd
dieghino26 <brotzadiego@gmail.com>
2024-02-20 22:29:56 +0100

    finisced snack1

--- snack1/index.php
+++ snack1/index.php
@@ -42,3 +42,20 @@ $matches = [
     ]
 ]
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php for ($i=0; $i < count($matches); $i++) :?>
+            <li>
+                <?=$matches[$i]["home_team"]?> - <?=$matches[$i]["away_team"]?> | <?=$matches[$i]["ht_point"]?>-<?=$matches[$i]["at_point"]?>
+            </li>
+        <?php endfor?>
+    </ul>
+</body>
+</html>
