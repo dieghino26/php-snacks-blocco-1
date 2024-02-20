b19e0279f88f2e68e76e043aea0ffeb4342ecc0d
dieghino26 <brotzadiego@gmail.com>
2024-02-20 22:12:59 +0100

    first commit

---
+++ snack1/index.php
@@ -0,0 +1,11 @@
+<?php
+/* Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
+Ogni partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60 */
+
+
+$matches = [
+    []
+]
+?>
